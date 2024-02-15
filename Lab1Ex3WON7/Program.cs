e423799d7760a145ff3569531ebba89f4d59e1f3
codri <codri@VIRTUAL>
2024-02-15 22:43:05 +0200

    Add project files.

---
+++ Lab1Ex3WON7/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Ex3 
+Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la 
+tastaura
+ (google: how to get the last digit of a number)
+*/
+const uint cifre = 1;
+ShowLastDigits(ReadNumber(cifre));
---
+++ Lab1Ex3WON7/ProgramHelpers.cs
@@ -0,0 +1,24 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+partial class Program
+{
+    static (int,uint) ReadNumber(uint cifre)
+    {
+        int numar;
+        Console.WriteLine($"Scrieti un numar intreg pentru a i se afisa ultimele {cifre} {nameof(cifre)}");
+        if (!int.TryParse(Console.ReadLine(), out numar))
+        {
+            Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+            ReadNumber(cifre);
+        }
+        return (numar,cifre);
+    }
+    static void ShowLastDigits((int numar,uint cifre) date)
+    {
+        var numarString = date.numar.ToString();
+        for (int i = (int)(numarString.Length - date.cifre); i < numarString.Length; i++) 
+        {
+            Console.Write(numarString[i]);
+        }
+    }
+}
