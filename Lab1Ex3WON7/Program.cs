ce901367cc2cefd2b589d4a042629a11346ce65f
codrinursachi <codrinursachi@gmail.com>
2024-02-17 09:31:18 +0200

    Formatting

--- Lab1Ex3WON7/Program.cs
+++ Lab1Ex3WON7/Program.cs
@@ -3,5 +3,5 @@ Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
 tastaura
  (google: how to get the last digit of a number)
 */
-const uint cifre = 1;
-ShowLastDigits(ReadNumber(cifre));
+const uint NUMAR_CIFRE = 1;
+ShowLastDigits(ReadNumber(NUMAR_CIFRE));
