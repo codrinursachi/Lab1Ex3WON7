e20f06fb154ada7ff2d4afbe85ef49d2d2a530a7
codrinursachi <codrinursachi@gmail.com>
2024-02-18 08:26:20 +0200

    Formatting

---
+++ Lab1Ex3WON7/Program.Helpers.cs
@@ -0,0 +1,19 @@
+﻿partial class Program
+{
+    static (int,uint) ReadNumber(in uint NUMAR_CIFRE)
+    {
+        int numar;
+        Console.WriteLine($"Scrieti un numar intreg pentru a i se afisa ultimele {NUMAR_CIFRE} cifre");
+        if (!int.TryParse(Console.ReadLine(), out numar))
+        {
+            Console.WriteLine("Nu ati introdus datele in format valid, mai incercati");
+            ReadNumber(NUMAR_CIFRE);
+        }
+        return (numar, NUMAR_CIFRE);
+    }
+    static void ShowLastDigits(in (int numar,uint NUMAR_CIFRE) date)
+    {
+        var numarString = date.numar.ToString();
+        Console.WriteLine(numarString.Substring((int)(numarString.Length - date.NUMAR_CIFRE)));
+    }
+}
